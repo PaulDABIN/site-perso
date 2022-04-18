bee8e5da3ef6e58aec8cabad35407d801ebbc5c6
PaulDABIN <dabin.paul@gmail.com>
2022-04-18 17:05:55 +0200

    Create Header.js

--- src/components/molecules/Header.js
+++ src/components/molecules/Header.js
@@ -1,24 +1,34 @@
-import React from 'react'
-import styled from 'styled-components'
-import { theme } from '../../theme/index';
+import React from "react"
+import styled from "styled-components"
+import { theme } from "../../theme/index"
+
+import Languages from "./Languages"
 
 export default function Header() {
-    return (
-        <header>
-            <nav>
-                <li>Acueil</li>
-                <li>A propos</li>
-                <li>Services</li>
-                <li>Portfolio</li>
-                <li>Contact</li>
-            </nav>
-
-        </header>
-    )
+  return (
+    <HeaderStyled>
+      <LogoStyled>
+        <span>Logo</span>
+      </LogoStyled>
+      <nav>
+        <a>Acueil</a>
+        <a>A propos</a>
+        <a>Services</a>
+        <a>Portfolio</a>
+        <a>Contact</a>
+        <Languages />
+      </nav>
+    </HeaderStyled>
+  )
 }
 
-/*const HeaderStyled = styled.header`
-    color : blue;
-
-
-`*/
+const HeaderStyled = styled.header`
+  color: blue;
+  display: flex;
+  flex-direction: row-reverse;
+  max-width: 1200px;
+`
+const LogoStyled = styled.div`
+  text-align: left;
+  background-color: red;
+`
--- src/components/pages/Accueil.js
+++ src/components/pages/Accueil.js
@@ -1,18 +1,16 @@
-import { Link } from '@reach/router'
-import React from 'react'
+import { Link } from "@reach/router"
+import React from "react"
 import { useState } from "react"
-import styled from 'styled-components'
+import styled from "styled-components"
 
-
-import Languages from '../molecules/Languages'
-import Presentation from '../molecules/Presentation'
-import Header from '../molecules/Header'
-
+import Languages from "../molecules/Languages"
+import Presentation from "../molecules/Presentation"
+import Header from "../molecules/Header"
 
 export default function Accueil() {
-    //state
+  //state
 
-    /*
+  /*
     //component
     const handleSubmit = (event) => {
         event.preventDefault() //permet d'empecher l'évènement par defaut du onSubmit 
@@ -20,17 +18,20 @@ export default function Accueil() {
         //setInputValue("")
     }*/
 
-    
-    //render
-    return(
+  //render
+  return (
+    <MainStyled>
+      <Header />
 
-    <div>
-      <Header/>
-      <Languages/>
-      <Presentation/>
-    </div>
-
-
-    )
-
+      <Presentation />
+    </MainStyled>
+  )
 }
+
+const MainStyled = styled.div`
+  border: 1px solid grey;
+  max-width: 1200px;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+`
